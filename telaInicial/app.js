daf65abf609ad2141791bc4462f1767d44087091
anderson-do-ponto <118007089+anderson-do-ponto@users.noreply.github.com>
2023-10-05 17:00:10 -0300

    finalização script professores e alunos

--- telaInicial/app.js
+++
@@ -1,9 +0,0 @@
-function abrirMenu (){
-    const lista = document.getElementById("uluser")
-
-    if (lista.style.display === "none" || lista.style.display === "") {
-        lista.style.display = "block"
-    } else {
-        lista.style.display = "none"
-    }
-}
